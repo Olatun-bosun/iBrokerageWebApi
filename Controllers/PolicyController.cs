213ad2761436b9ffcfe590df34a49306793af413
Your Nam <you@example.com>
2024-03-26 12:53:25 +0100

    Controller Change

--- Controllers/PolicyController.cs
+++ Controllers/PolicyController.cs
@@ -1,8 +1,7 @@
-﻿using iBrokerageWebApi.Data;
-using iBrokerageWebApi.Model.Domain;
+﻿using iBrokerageWebApi.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Runtime.Serialization.DataContracts;
+using System.Data.SqlClient;
 
 namespace iBrokerageWebApi.Controllers
 {
@@ -10,16 +9,95 @@ namespace iBrokerageWebApi.Controllers
     [ApiController]
     public class PolicyController : ControllerBase
     {
-        private readonly DataContext dbContext;
-        public PolicyController(DataContext dbContext)
+        private readonly string connectionString;
+
+        public PolicyController(IConfiguration configuration)
         {
-            this.dbContext = dbContext;
+            connectionString = configuration["ConnectionStrings:SqlServerDb"] ?? "";
         }
-        [HttpGet]
-        public IActionResult GetAll()
+
+        [HttpPost]
+        public IActionResult CreatePolicy(Policy policy)
         {
-            var policies = dbContext.Policies.ToList();
-            return Ok(policies);
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "INSERT INTO policies " +
+                        "(PolicyNo, CoPolicyNo, BranchID, Branch, BizSource) VALUES " +
+                        "(@PolicyNo, @CoPolicyNo, @BranchID, @Branch, @BizSource)";
+
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("PolicyNo", policy.PolicyNo);
+                        command.Parameters.AddWithValue("CoPolicyNo", policy.CoPolicyNo);
+                        command.Parameters.AddWithValue("BranchID", policy.BranchID);
+                        command.Parameters.AddWithValue("Branch", policy.Branch);
+                        command.Parameters.AddWithValue("BizSource", policy.BizSource);
+
+
+                        command.ExecuteNonQuery();
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Product", "Sorry but we have an exception");
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
         }
+
+        [HttpGet("{PolicyNo}")]
+        public IActionResult GetPolicy(string PolicyNo)
+        {
+            Policy policy = new Policy();
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM Policies WHERE PolicyNo=@PolicyNo";
+
+
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("PolicyNo", PolicyNo);
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+
+                                policy.PolicyNo = reader.GetString(1);
+                                policy.CoPolicyNo = reader.GetString(2);
+                                policy.BranchID = reader.GetString(3);
+                                policy.Branch = reader.GetString(4);
+                                policy.BizSource = reader.GetString(5);
+
+
+
+                            }
+                            else
+                            {
+                                return NotFound();
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Product", "Sorry but we have an exception");
+                return BadRequest(ModelState);
+            }
+
+            return Ok(policy);
+        }
+
     }
 }
---
+++ Model/Policy.cs
@@ -0,0 +1,11 @@
+﻿namespace iBrokerageWebApi.Model
+{
+    public class Policy
+    {
+        public string PolicyNo { get; set; } = "";
+        public string CoPolicyNo { get; set; } = "";
+        public string BranchID { get; set; } = "";
+        public string Branch { get; set; } = "";
+        public string BizSource { get; set; } = ""; 
+    }
+}
